f3bea0cc6a2b5b4f42e67fd81154aad91be19f75
ankita459 <ankitapanpatil18pc@student.mes.ac.in>
2023-02-02 15:59:20 +0530

    Add files via upload

---
+++ Dashboard/src/pages/GroupComponent.js
@@ -0,0 +1,234 @@
+import "./GroupComponent.css";
+
+const GroupComponent = () => {
+  return (
+    <div className="dashboard-parent">
+      <div className="dashboard">
+        <img className="dashboard-child" alt="" src="../rectangle-6627.svg" />
+        <b className="month-year">Welcome Tina !</b>
+        <div className="important-announcements">Important Announcements</div>
+        <div className="rectangle-parent">
+          <div className="group-child" />
+          <img
+            className="vuesaxlineararrow-right-icon"
+            alt=""
+            src="../vuesaxlineararrowright.svg"
+          />
+          <div className="english-classes-parent">
+            <div className="english-classes">English classes</div>
+            <div className="tika-sarak-spd">Tika sarak s.pd</div>
+          </div>
+          <img className="group-item" alt="" src="../ellipse-114.svg" />
+        </div>
+        <div className="rectangle-group">
+          <div className="group-child" />
+          <img
+            className="vuesaxlineararrow-right-icon"
+            alt=""
+            src="../vuesaxlineararrowright1.svg"
+          />
+          <div className="english-classes-parent">
+            <div className="english-classes">English classes</div>
+            <div className="tika-sarak-spd">Tika sarak s.pd</div>
+          </div>
+          <img className="group-item" alt="" src="../ellipse-114.svg" />
+        </div>
+        <div className="rectangle-container">
+          <div className="group-child" />
+          <img
+            className="vuesaxlineararrow-right-icon"
+            alt=""
+            src="../vuesaxlineararrowright1.svg"
+          />
+          <div className="mathematic-classes-parent">
+            <div className="english-classes">Mathematic classes</div>
+            <div className="tika-sarak-spd">Tika sarak s.pd</div>
+          </div>
+          <img className="group-item" alt="" src="../ellipse-114.svg" />
+        </div>
+        <div className="group-div">
+          <div className="group-child" />
+          <img
+            className="vuesaxlineararrow-right-icon"
+            alt=""
+            src="../vuesaxlineararrowright1.svg"
+          />
+          <div className="mathematic-classes-parent">
+            <div className="english-classes">Mathematic classes</div>
+            <div className="tika-sarak-spd">Tika sarak s.pd</div>
+          </div>
+          <img className="group-item" alt="" src="../ellipse-114.svg" />
+        </div>
+        <div className="rectangle-parent1">
+          <div className="group-child" />
+          <img
+            className="vuesaxlineararrow-right-icon"
+            alt=""
+            src="../vuesaxlineararrowright1.svg"
+          />
+          <div className="physical-classes-parent">
+            <div className="english-classes">Physical classes</div>
+            <div className="tika-sarak-spd">Tika sarak s.pd</div>
+          </div>
+          <img className="group-item" alt="" src="../ellipse-114.svg" />
+        </div>
+        <div className="rectangle-parent2">
+          <div className="group-child" />
+          <img
+            className="vuesaxlineararrow-right-icon"
+            alt=""
+            src="../vuesaxlineararrowright1.svg"
+          />
+          <div className="physical-classes-parent">
+            <div className="english-classes">Physical classes</div>
+            <div className="tika-sarak-spd">Tika sarak s.pd</div>
+          </div>
+          <img className="group-item" alt="" src="../ellipse-114.svg" />
+        </div>
+        <div className="dashboard-item" />
+        <img
+          className="logoipsum-logo-8-1-icon"
+          alt=""
+          src="../logoipsumlogo8-1.svg"
+        />
+        <div className="vuesaxboldlogout-parent">
+          <img
+            className="vuesaxboldlogout-icon"
+            alt=""
+            src="../vuesaxboldlogout.svg"
+          />
+          <div className="logout">Logout</div>
+        </div>
+        <div className="dashboard-inner" />
+        <div className="new-feed-parent">
+          <div className="new-feed">New feed</div>
+          <img
+            className="vuesaxboldelement-4-icon"
+            alt=""
+            src="../vuesaxboldelement4.svg"
+          />
+        </div>
+        <div className="services-parent">
+          <div className="new-feed">Services</div>
+          <img
+            className="vuesaxboldelement-4-icon"
+            alt=""
+            src="../vuesaxboldcalendar.svg"
+          />
+        </div>
+        <div className="dashboard-inner1">
+          <div className="projects-parent">
+            <div className="new-feed">Projects</div>
+            <img
+              className="vuesaxboldelement-4-icon"
+              alt=""
+              src="../vuesaxboldbriefcase.svg"
+            />
+          </div>
+        </div>
+        <div className="employees-parent">
+          <div className="new-feed">Employees</div>
+          <img
+            className="vuesaxboldelement-4-icon"
+            alt=""
+            src="../vuesaxboldsms.svg"
+          />
+        </div>
+        <div className="careers-parent">
+          <div className="new-feed">Careers</div>
+          <img
+            className="vuesaxboldelement-4-icon"
+            alt=""
+            src="../vuesaxboldprofile2user.svg"
+          />
+        </div>
+        <div className="policies-parent">
+          <div className="new-feed">Policies</div>
+          <img
+            className="vuesaxboldelement-4-icon"
+            alt=""
+            src="../vuesaxboldsetting2.svg"
+          />
+        </div>
+        <div className="feedback-parent">
+          <div className="new-feed">Feedback</div>
+          <img
+            className="vuesaxboldelement-4-icon"
+            alt=""
+            src="../vuesaxboldsetting21.svg"
+          />
+        </div>
+        <div className="dashboard-inner2">
+          <div className="careers-wrapper">
+            <div className="careers1">Careers</div>
+          </div>
+        </div>
+        <div className="rectangle-parent3">
+          <div className="group-child8" />
+          <img
+            className="vuesaxlinearsearch-normal-icon"
+            alt=""
+            src="../vuesaxlinearsearchnormal.svg"
+          />
+          <div className="search">Search</div>
+        </div>
+        <img className="rectangle-icon" alt="" src="../rectangle-1072@2x.png" />
+        <img className="group-icon" alt="" src="../group-47875.svg" />
+        <div className="card-6">
+          <div className="card-61">
+            <div className="card-6-child" />
+            <div className="card-6-item" />
+          </div>
+          <b className="fronend-developer">Fronend Developer</b>
+          <b className="skills">{`Skills `}</b>
+          <b className="experience">Experience :</b>
+          <div className="lorem-ipsum-dolor">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar
+            nisl ipsum morbi diam scelerisque pulvinar elementum, viverra.
+          </div>
+          <div className="new-btn-parent">
+            <div className="new-btn">
+              <div className="new-btn-child" />
+              <b className="new">New</b>
+            </div>
+            <div className="new-btn1">
+              <div className="new-btn-child" />
+              <b className="new">New</b>
+            </div>
+            <div className="new-btn2">
+              <div className="new-btn-child" />
+              <b className="new">New</b>
+            </div>
+            <div className="new-btn3">
+              <div className="new-btn-child" />
+              <b className="new">New</b>
+            </div>
+            <div className="new-btn4">
+              <div className="new-btn-child" />
+              <b className="new">New</b>
+            </div>
+          </div>
+        </div>
+        <div className="dashboard-inner3">
+          <div className="careers-wrapper">
+            <div className="group-child9" />
+          </div>
+        </div>
+        <img
+          className="iconlylightarrow-down-2"
+          alt=""
+          src="../iconlylightarrow--down-2.svg"
+        />
+        <div className="trending-card" />
+        <div className="frame-div" />
+        <b className="upcoming-events">Upcoming Events</b>
+        <div className="one-of-the">
+          One of the most recently discovered ruins, the historians till date
+          ...
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GroupComponent;
